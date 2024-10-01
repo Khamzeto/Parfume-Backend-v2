d16622da7f30aab4f55143da52dfa81cc4cb437b
Flamesyyyyy <147990355+Flamesyyyyy@users.noreply.github.com>
2024-10-01 18:47:11 +0300

    ff

--- src/models/noteModel.ts
+++ src/models/noteModel.ts
@@ -3,10 +3,12 @@ import mongoose, { Document, Schema } from 'mongoose';
 // Интерфейс для ноты
 export interface INote extends Document {
   name: string;
+  image?: string; // Опциональное поле для изображения
 }
 
 const noteSchema: Schema = new Schema({
   name: { type: String, required: true, unique: true }, // уникальное поле для каждой ноты
+  image: { type: String }, // Поле для хранения URL изображения
 });
 
 export default mongoose.model<INote>('Note', noteSchema);
