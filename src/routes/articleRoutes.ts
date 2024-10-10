246e2a516c0915d136f995b123c164f67c301c21
Хамзат Хамзатов <khamzat@MacBook-Pro-Hamzat.local>
2024-10-11 00:06:36 +0300

    fsdfsd

--- src/routes/articleRoutes.ts
+++ src/routes/articleRoutes.ts
@@ -7,6 +7,7 @@ import {
   deleteArticleRequest,
   getArticleRequestsByUserId,
   updateArticleRequest,
+  getApprovedArticleRequestsByUserId,
 } from '../controllers/articleController'; // Импорт контроллеров
 
 const router = Router();
